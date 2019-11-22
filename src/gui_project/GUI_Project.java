4c7cbb1685db15ddd5a4b4d57d7fa248df2d2ad0
Dhriti Adyanthaya <dhritiadyanthaya14@gmail.com>
2019-11-22 14:55:09 +0400

    Update GUI_Project.java
    basic frame

--- src/gui_project/GUI_Project.java
+++ src/gui_project/GUI_Project.java
@@ -5,15 +5,23 @@
  */
 package gui_project;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.GridLayout;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 /**
  *
  * @author Zaid
  */
 public class GUI_Project {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("This is a test..");
@@ -24,6 +32,68 @@ public class GUI_Project {
         System.out.println("Wow this is amazing. Thanks Azadan");
         System.out.println("Zaid told me say, \"Abdullah added this\" and then we all have to pull (god knows what that means)");
         System.out.println("Github is a bloody racist");
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+            GameFrame frame = new GameFrame();
+                frame.setVisible(true);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
+                frame.setTitle("Game Of Life");
+                frame.setSize(500, 500);
+                frame.getContentPane().setBackground(Color.LIGHT_GRAY);
+            
+                
+                
+            }
+        });
+        
     }
     
+}
+
+
+class GameFrame extends JFrame{
+
+    public GameFrame() {
+        setBackground(Color.LIGHT_GRAY);
+         JPanel boxes = new JPanel();
+         boxes.setLayout(new GridLayout(25,25));
+         boxes.setBackground(Color.DARK_GRAY);
+      
+         
+         for (int i = 0; i < 500; i++) {
+             JLabel grids = new JLabel(" ");
+             grids.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+             boxes.add(grids);
+        }
+         
+         
+         
+         add(boxes, BorderLayout.CENTER);
+         GamePanels controllers = new GamePanels();
+         add(controllers, BorderLayout.SOUTH);
+    }
+     
+}
+
+class GamePanels extends JPanel{
+    public GamePanels(){
+        
+        setBackground(Color.LIGHT_GRAY);
+        String[] Shapes = {"Glider", "R-Pentomino","Small Exploder", "Small spaceship", "Tumbler"};
+        add(new JComboBox(Shapes));
+        
+        add(new JButton("Next"));
+        add(new JButton("Start"));
+        
+        String[] Speed = {"Slow", "Normal","Fast"};
+        add(new JComboBox(Speed));
+        
+        JLabel generation = new JLabel();
+        generation.setText("Generation: ");
+        add(generation);
+
+        }
+
+
 }
